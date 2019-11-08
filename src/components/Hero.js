4d885d3acaad7ed4bd5ed6c93fe2e6701690021e
ashok0092 <ashok@makemywebsite.com.au>
2019-11-08 11:14:11 +0530

    hero component update, delete unused files, project urls support

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,43 +9,15 @@ import heroMap from "../images/hero-map.png"
 const Wrap = styled.div({
   height: "100vh",
   maxHeight: "50vh",
-  backgroundImage: `url(${heroBg})`,
-  backgroundSize: "cover",
-  display: "flex",
-  alignItems: "center",
+  backgroundColor: `#000`,
   color: `#fff`,
   position: "relative",
 })
 
-const Content = styled(Row)({
-  zIndex: 1,
-})
-
-const DarkOverlay = styled.div({
-  position: "absolute",
-  top: 0,
-  left: 0,
-  width: `100%`,
-  height: `100%`,
-  backgroundColor: "rgba(0,0,0,0.66)",
-})
-
-const Left = styled.div({
-  flex: "1 1 auto",
-  float: "left",
-  width: "60%",
-})
-
-const Image = styled.img({
-  flex: "0 0 auto",
-  float: "right",
-  width: "40%",
-})
-
 const Hero = () => (
   <Wrap>
-    <DarkOverlay />
-    <Content>
+    <Content className="test">
+      <p>Hello</p>
       <Left>
         <h1>Speak with Our Specialists to expand your social reach</h1>
         <p>
--- src/content/projects.js
+++ src/content/projects.js
@@ -4,23 +4,27 @@ module.exports = [
     title: "Green Landscapes",
     image: "portfolio/greenlandscapes.jpg",
     tags: ["Landscaping", "Ecommerce"],
+    url: "https://www.greenlandscapes.com.au",
   },
   {
     path: "imaging-specialists",
     title: "Imaging Specialists",
     image: "portfolio/imagingspecialists.jpg",
     tags: ["Imaging", "Ecommerce"],
+    url: "https://www.imagingspecialists.com.au",
   },
   {
     path: "bouquet-cakes-by-tiffany",
     title: "Bouquet Cakes By Tiffany",
     image: "portfolio/bouquetcakesbytiffany.jpg",
     tags: ["Cakes", "Ecommerce"],
+    url: "https://www.bouquetcakesbytiffany.com.au/",
   },
   {
     path: "popular-care",
     title: "Popular Care",
     image: "portfolio/popularcare.jpg",
     tags: ["Healthcare"],
+    url: "https://www.popularcare.com.au",
   },
 ]
