07c4b176a60fb9ca021e53ce1042afc1e742505c
ashok0092 <ashok@makemywebsite.com.au>
2019-11-14 18:30:51 +0530

    Update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Make My Website`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `Make My Website is an affordable Web Design company located in Melbourne, Australia. We have been mastering the art of Web Design, SEO, Development and SMM`,
     author: `@gatsbyjs`,
   },
   plugins: [
@@ -33,7 +33,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
