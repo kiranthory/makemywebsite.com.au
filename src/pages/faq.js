a039fea7f467d517413e343526af6f8c16a505df
Kiran Thory <thorykiran@gmail.com>
2019-11-20 16:07:50 +0530

    Faq Page

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -4,14 +4,15 @@ import Layout from "../components/Layout"
 import Seo from "../components/Seo"
 import styled from "@emotion/styled"
 import Accordion from "../components/Accordion"
+import { P } from "../components/serviceComponents"
 const FaqWrap = styled(Row)({})
 
 const Faq = () => (
   <Layout>
     <Seo title="Frequently Asked Questions - Make My Website" />
     <FaqWrap>
-      <Accordion title="Hello">
-        <p>
+      <Accordion title="What we do?">
+        <P>
           Make My Website specialise in designing, developing and advertising
           websites for small to medium organisations all over Australia. We also
           provide hosting service, domain registration and online marketing
@@ -22,31 +23,140 @@ const Faq = () => (
           most out of your site. Also, once the website is live, we continue to
           work effectively with you to accomplish your business objectives and
           online goals.
-        </p>
+        </P>
       </Accordion>
-      <Accordion title="Is Make My website a right company for my budget?">
-        <p>
+      <Accordion title="	Is Make My website a right company for my budget?">
+        <P>
           Our websites range start from $599 and can reach to $8,000 or more. It
           completely depends upon the customer requirements. We deliver more
           than what we promised and always exceeded customer expectations. We
           are proud to be the right choice for small to medium businesses. Your
           every dollar is invested to give your business an online presence with
-          researches keywords so that your clients can reach you easily. Our
-          Prices are always in your budget, MMW offers affordable costs with
+          researches keywords so that your clients can reach you easily.
+        </P>
+        <P>
+          Our Prices are always in your budget, MMW offers affordable costs with
           better websites.
-        </p>
+        </P>
       </Accordion>
-      {/* <ButtonItem>
-        What's included in Website build by Make My Website?
-      </ButtonItem>
-      <AccorionDiv>
-        <p>
+      <Accordion title="		What's included in Website build by Make My Website?">
+        <P>
           Whether you want us to build a new website or rebuild or upgrade the
           existing website, we are happy to assist you. We provide multiple
           design packages that ideally cater to the needs of a wide range of
           small and medium size to enterprise level website design projects.
-        </p>
-        <p>Our websites include (as per customer requirements) :</p>
+        </P>
+        <P>Our websites include (as per customer requirements) :</P>
+        <ul>
+          <li>Web pages</li>
+          <li>Responsive Designs</li>
+          <li>Stock Images</li>
+          <li>Smart phone,Call to Action</li>
+          <li>Enquiry Form</li>
+          <li>Social Media Icons</li>
+          <li>Content Upload</li>
+          <li>Photo Gallery</li>
+          <li>Slider</li>
+          <li>On Page SEO</li>
+          <li>Custom Form</li>
+          <li>Google Map Integration</li>
+          <li>Blog</li>
+          <li>News Letter Sign Up</li>
+          <li>Testimonials</li>
+          <li>Upgraded Graphic Buttons with Hover Transitions</li>
+          <li>Content Styling and Graphics</li>
+          <li>Shopping Cart</li>
+          <li>Payment Gateway</li>
+          <li>Site Search</li>
+          <li>Products</li>
+          <li>Client Login</li>
+          <li>Product Ratings</li>
+        </ul>
+      </Accordion>
+      <Accordion title="	Where are we based?">
+        <P>
+          We’re a Melbourne based company, with an office at 129A, Watton
+          Street, Werribee, Victoria. We have been catering to clients all over
+          Australia as we have our presence in almost every state of Australia,
+          including the major cities like Sydney, Brisbane, etcetera. We would
+          be more than happy to assist you at our office in Werribee, or else we
+          are just a call away to create your dream website.
+        </P>
+      </Accordion>
+      <Accordion title="	Do MMW build all the websites on WordPress?">
+        <P>
+          No, We build websites on all platforms. However, we recommend
+          WordPress because this is most powerful, CMS robust that gives our
+          developers and our clients to have control over websites. If you want
+          your website to work optimally and perform well on google, WordPress
+          will be the right choice for you.
+        </P>
+      </Accordion>
+      <Accordion title="Is it possible to make changes once the website is live?">
+        <P>
+          Yes, Of course! You can do minor changes by yourself. You can add
+          pictures, add/change text, modify minor designing. We will assist you
+          with how to manage your website by yourself.
+        </P>
+        <P>
+          But In case, if you want us to manage your website. We would be more
+          than happy to do it for you. Our monthly charges will be $50 per
+          month.
+        </P>
+      </Accordion>
+      <Accordion title="	Do you design Logo too? Is Logo Important?">
+        <P>
+          Yes, we do create a incredible logo for your business or brand at a
+          very minimal cost. And Logo is very important because it represents
+          recognition to your organisation.
+        </P>
+      </Accordion>
+      <Accordion title="What are MMW payment plans?">
+        <P>
+          We charge 50% of the amount quoted at the commencement of your website
+          and will charge rest 50% amount on the completion of your website but
+          once you will be fully satisfied with your website.
+        </P>
+      </Accordion>
+      <Accordion title="Can you Upgrade/re-build my existing website?">
+        <P>
+          Absolutely, We can do that. We ensure we will come up far better and
+          outstanding website.
+        </P>
+      </Accordion>
+      <Accordion title="	Is it necessary to host a website with MMW?">
+        <P>
+          No, it’s not necessary. However, we prefer our customers to host their
+          websites with MMW so that we can provide the continuous management and
+          security of your website. It will be our responsibility to make sure
+          working of your website and all server related issues.
+        </P>
+      </Accordion>
+      <Accordion title="How my website will rank higher on Google and all other search engines? Is SEO important?">
+        <P>
+          Getting website is the first step to make your online presence on web,
+          but definitely is not enough to rank higher on Google and other search
+          engines. In a competitive online market, Search Engine Optimisation
+          helps you organically rank higher using specific keywords relating
+          your business. If your website rank high, it automatically grabs the
+          attention of prospective clients. Our SEO experts are happy to assist
+          you.
+        </P>
+      </Accordion>
+      <P>
+        <b>Please Get In Touch Today for more queries!</b>
+      </P>
+      {/* <ButtonItem>
+        What's included in Website build by Make My Website?
+      </ButtonItem>
+      <AccorionDiv>
+        <P>
+          Whether you want us to build a new website or rebuild or upgrade the
+          existing website, we are happy to assist you. We provide multiple
+          design packages that ideally cater to the needs of a wide range of
+          small and medium size to enterprise level website design projects.
+        
+        <P>Our websites include (as per customer requirements) :
         <ul class="faq-list">
           <li>Web pages</li>
           <li>Responsive Designs</li>
@@ -75,14 +185,14 @@ const Faq = () => (
       </AccorionDiv>
       <ButtonItem> Where are we based? </ButtonItem>
       <AccorionDiv>
-        <p>
+        <P>
           We’re a Melbourne based company, with an office at 129A, Watton
           Street, Werribee, Victoria. We have been catering to clients all over
           Australia as we have our presence in almost every state of Australia,
           including the major cities like Sydney, Brisbane, etcetera. We would
           be more than happy to assist you at our office in Werribee, or else we
           are just a call away to create your dream website.
-        </p>
+        
       </AccorionDiv>
       <ButtonItem> Do MMW build all the websites on WordPress?</ButtonItem>
 
